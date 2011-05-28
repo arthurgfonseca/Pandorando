85bbe5c204a3953b0c2a9c2a82b29e6fce8679bb
Arthur <arthur.fonseca@i.ndigo.com.br>
2011-05-28 12:06:16 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pandorando::Application.config.secret_token = '4ada952dc5b01795ff0d216bb7113fa7019a93aea915cfdace651592dd82a748bc71ad2065811e69a8a6040743d1f785d061bd7445520563434c56303fe4185a'
