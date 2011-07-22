cd121fc32f9e0e3f87044fa0f11dfddb30e19f77
Arthur <arthur.fonseca@i.ndigo.com.br>
2011-07-21 21:32:35 -0300

    algoritmo

---
+++ app/models/constants.rb
@@ -0,0 +1,11 @@
+class Constants
+  
+  # Constants to calculate the soa algorithm
+  NUMBER_OF_NEURONS_FOR_ROW = 10
+  NEIGHBOURHOOD_RADIUS = (NUMBER_OF_NEURONS_FOR_ROW / 5)
+  TIME_CONSTANT = 1.5
+  LEARNING_RATE = 0.5
+  LEARNING_RATE_CONSTANT = 1.5
+  
+  
+end
---
+++ test/unit/helpers/som_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class SomHelperTest < ActionView::TestCase
+end
